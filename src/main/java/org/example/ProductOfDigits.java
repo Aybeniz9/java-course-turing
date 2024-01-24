38e1649b375eda161302988d21d93e955d83876e
aybeniz <aybenizkazimova15@gmail.com>
2024-01-24 18:23:06 +0400

    101010

---
+++ src/main/java/org/example/ProductOfDigits.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int firstDigit = num / 100;
+
+        int secondDigit = (num / 10) % 10;
+
+
+        int thirdDigit = num % 10;
+        System.out.println(firstDigit * secondDigit * thirdDigit);
+    }
+}
---
+++ src/main/java/org/example/SeperatedNumbers.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SeperatedNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        if(i<=99&&i>=10){
+            int firstDigit = i / 10;
+            int secondDigit = i % 10;
+            System.out.println(firstDigit+" "+secondDigit);
+        }
+        else{
+            System.out.println("This number invalid");
+        }
+    }
+}
