61d08687d6760887ab539286e2c71601c158cf96
aybeniz <aybenizkazimova15@gmail.com>
2024-01-26 19:17:03 +0400

    101010

--- src/main/java/org/example/COnditionalStateament2.java
+++ src/main/java/org/example/COnditionalStateament2.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class COnditionalStateament2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int y = 0;
+        long n = sc.nextInt();
+        long y = 0;
         if (n <= Math.pow(10, 4) && n >= Math.pow(10, 4) * (-1)) {
             if (n >= 10) {
                 y = n * n * n + n * 5;
