890d93ef69380848f509b7d9d3b58e2f7d43b4e4
aybeniz <aybenizkazimova15@gmail.com>
2024-02-02 04:35:21 +0400

    101010

--- src/main/java/org/example/EvenOrOdd.java
+++ src/main/java/org/example/EvenOrOdd.java
@@ -1,19 +1,19 @@
 package org.example;
+
 import java.util.Scanner;
+
 public class EvenOrOdd {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n <= Math.pow(10, 9) && n>=1){
-            if (n%2==0){
+        if (n <= Math.pow(10, 9) && n >= 1) {
+            if (n % 2 == 0) {
                 System.out.println("EVEN");
-            }
-            else {
+            } else {
                 System.out.println("ODD");
             }
-        }
-else {
+        } else {
             System.out.println("nvalid number");
         }
     }
